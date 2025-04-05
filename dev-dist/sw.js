94976805a97cbeb862a4d47b8b7b1ad76569faeb
nurdamiron <nurdamiron@gmail.com>
2025-04-05 13:20:15 +0500

    fue

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-25659a9f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.atdm3g7391o"
+    "revision": "0.gh8rqe5sbe8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
